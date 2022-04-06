9452a1b1d4e8bf68b420686053ec3d9c43411ee3
TMerlo02 <tmerlo@udesa.edu.ar>
2022-04-06 17:56:11 -0300

    sin cambios, no aparece lo hecho en el anterior

--- trabajoIntegrador/db/data.js
+++ trabajoIntegrador/db/data.js
@@ -6,7 +6,7 @@ const data = {
         id : 34,
         nombre : "camisas",
         img : "https://http2.mlstatic.com/D_NQ_NP_2X_634834-MLA46724971388_072021-F.webp"
-    }, , , , , , , , , , ],
+    }],
     comentarios : [ , , , , , , , , , ]
 }
 
