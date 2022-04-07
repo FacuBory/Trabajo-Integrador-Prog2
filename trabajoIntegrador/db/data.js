1a2559b3f1b68c7ba9af4aa4535bd22e12b302ea
Facundo Bory <facubory@MacBook-Pro-de-Facundo.local>
2022-04-07 14:31:02 -0300

    Pusimos las imagenes de los productos

--- trabajoIntegrador/db/data.js
+++ trabajoIntegrador/db/data.js
@@ -7,42 +7,50 @@ const data = {
         id : 1,
         nombre : "Cafetera Moulinex",
         img : "https://macfixstore.com/wp-content/uploads/2021/11/iphone-13-pink-select-2021.png",
+        descripción : " " , 
 
     },
     {
         id : 2,
         nombre : "Macbook Pro 2019",
-        img : ""
+        img : "https://istoreve.com/wp-content/uploads/2020/11/Macbook-pro-2019.png", 
+        descripción : " " , 
     },
     {
         id : 3,
         nombre : "Samsung Galaxy S10",
-        img : ""
+        img : "https://images.samsung.com/latin/smartphones/galaxy-s10/buy/m-img-s10-prismgreen-01.png",
+        descripción : " " , 
     },
     {
         id : 4,
         nombre : "Samsung Samrt Tv",
-        img : ""
+        img : "https://images.samsung.com/is/image/samsung/p6pim/ar/un43t5300agczb/gallery/ar-fhd-t5300-un43t5300agczb-356627272?$720_576_PNG$",
+        descripción : " " , 
     },
     {
         id : 5,
         nombre : "Iphone 13 pro",
-        img : ""
+        img : "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-13-pro-family-hero?wid=940&hei=1112&fmt=png-alpha&.v=1644969385433",
+        descripción : " " , 
     },
     {
         id : 6,
         nombre : "Lenovo Thinkpad",
-        img : ""
+        img : "https://www.lenovo.com/medias/22wsp144sa2.png?context=bWFzdGVyfHJvb3R8MzE4NjQ2fGltYWdlL3BuZ3xoNDgvaDk2LzE0MTExNzA0NjEyODk0LnBuZ3xlZjc5YTg1NTNiM2Y1NWMyMzI0N2UyYWM0M2ViMWFkMGRiOTkwMmYzZGJjM2MxMjgxZGZlYWQzMTVkMjZkOTY1",
+        descripción : " " , 
     },
     {
         id : 7,
         nombre : "Heladera Samsung",
-        img : ""
+        img : "https://samsungar.vtexassets.com/arquivos/ids/164847-800-auto?width=800&height=auto&aspect=true",
+        descripción : " " , 
     },
     {
         id : 8,
-        nombre : "Ipad Pro",
-        img : ""
+        nombre : "Ipad Pro 12.9'",
+        img : "https://cbafederal.net/wp-content/uploads/2021/09/Apple-iPad-Pro-129-inch-5th-gen-Space-Gray-frontimage.png",
+          descripción : " " , 
     },
     
 ],
