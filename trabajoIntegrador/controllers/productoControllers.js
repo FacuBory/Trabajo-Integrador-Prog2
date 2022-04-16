a64712f696f93c058a7605e556421347fa9d5171
TMerlo02 <tmerlo@udesa.edu.ar>
2022-04-16 18:26:57 -0300

    rutas a detalle y product add hechas, falta estructura

--- trabajoIntegrador/controllers/productoControllers.js
+++ trabajoIntegrador/controllers/productoControllers.js
@@ -2,8 +2,16 @@
 let data = require('../db/data')
  
 let productoControllers ={
-    info: function(req, res, next) {
-        res.render('index',{listaProducto: data.productos[i]});
-      }
-}
+    info: function(req, res) {
+        res.render('product-add',{});
+      },
+
+    detalleProducto: function (req, res){
+        let idProducto = req.params.id
+        return res.render('products',{
+          producto: data.productos[idProducto],
+          comentarios: data.comentarios
+        })
+}}
+
 module.exports=productoControllers
--- trabajoIntegrador/routes/producto.js
+++ trabajoIntegrador/routes/producto.js
@@ -3,5 +3,6 @@ var router = express.Router();
 let productoRouter = require('../controllers/productoControllers')
 /* GET home page. */
 router.get('/product-add',productoRouter.info)
+router.get('/detalle',productoRouter.detalleProducto)
 
 module.exports = router;
