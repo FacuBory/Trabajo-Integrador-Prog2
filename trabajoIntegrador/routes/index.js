a14992de58ff0bbd333de26d712f261b98a119aa
TMerlo02 <tmerlo@udesa.edu.ar>
2022-04-17 22:50:42 -0300

    ruta de search results y vinculos en los botones terminado

--- trabajoIntegrador/routes/index.js
+++ trabajoIntegrador/routes/index.js
@@ -3,5 +3,6 @@ var router = express.Router();
 let indexRouter = require('../controllers/indexControllers')
 /* GET home page. */
 router.get('/index', indexRouter.index);
+router.get('/search-results', )
 
 module.exports = router;
