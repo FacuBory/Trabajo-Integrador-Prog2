29cb8dc7ab1ad21ef0f52c5588f3f3c6a4aa451e
Tomas merlo <tommymerlo99@gmail.com>
2022-06-17 02:20:50 -0300

    cretada la funcion create y store de product add, error al cargar el producto

--- trabajoIntegrador/controllers/productoControllers.js
+++ trabajoIntegrador/controllers/productoControllers.js
@@ -1,20 +1,50 @@
 
-let data = require('../database/models')
- 
-let productoControllers ={
-    info: function(req, res) {
-        res.render('product-add',{
-          usuarioLogueado: "dfdgf"
-        });
-      },
+const db = require('../database/models');
+let data = require('../database/models');
+const Producto = require('../database/models/Producto');
+let productoNuevo = db.Producto;
 
-    detalleProducto: function (req, res){
-      let idProducto = req.params.id
-        return res.render('products',{
-          detalleProducto: data.productos[idProducto],
-          comentarios: data.comentarios
-          
-        })
-}}
+let productoControllers = {
+  info: function (req, res) {
+    res.render('product-add', {
+      usuarioLogueado: "dfdgf"
+    });
+  },
 
-module.exports=productoControllers
+  detalleProducto: function (req, res) {
+    let idProducto = req.params.id
+    return res.render('products', {
+      detalleProducto: data.productos[idProducto],
+      comentarios: data.comentarios
+
+    })
+  },
+  create: (req,res) => {
+  if(req.session.user != undefined){
+    return res.render('register')
+  }else{
+    return res.render('login')
+  }
+  },
+
+  store: (req,res)=>{
+    let info = req.body
+    let productoGuardado = {
+    nombre: info.nombreProducto,
+    imagen: info.fotoDeProducto,
+    descripcion: info.descripcionProducto,
+    }
+    productoNuevo.create(
+      productoGuardado
+      )
+      console.log(productoGuardado)
+      .then((result) => {
+        return res.redirect("/")
+      })
+    }
+    
+  }
+
+  
+
+module.exports = productoControllers
--- trabajoIntegrador/routes/producto.js
+++ trabajoIntegrador/routes/producto.js
@@ -4,5 +4,9 @@ let productoControllers = require('../controllers/productoControllers')
 /* GET home page. */
 router.get('/product-add',productoControllers.info)
 router.get('/detalle/:id',productoControllers.detalleProducto)
+router.get('/users/register', productoControllers.create);
+
+router.post('/users/register', productoControllers.store);
+
 
 module.exports = router;
