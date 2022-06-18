7f045f2f08c24c4499a302a97b99b134e3f8ad4a
Tomas merlo <tommymerlo99@gmail.com>
2022-06-17 21:24:29 -0300

    problemas con multer, no puedo arreglar cookies por eso

--- trabajoIntegrador/app.js
+++ trabajoIntegrador/app.js
@@ -24,6 +24,8 @@ app.use(function(req, res, next) {
   }
   return next();
 })
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
--- trabajoIntegrador/controllers/userControllers.js
+++ trabajoIntegrador/controllers/userControllers.js
@@ -1,81 +1,83 @@
 var express = require('express')
 let db = require('../database/models');
 let user = db.Usuario;
-const bcryptjs = require ('bcryptjs');
+const bcryptjs = require('bcryptjs');
 
 const userController = {
-profile: function(req, res, next) {
+  profile: function (req, res, next) {
 
-  res.render('profile',{
-    usuarioLogueado:data.usuario,
-    listaProducto : data.productos
-  });
-},
+    res.render('profile', {
+      usuarioLogueado: data.usuario,
+      listaProducto: data.productos
+    });
+  },
 
-userDeslogueado: function(req, res, next) {
- 
-  res.render('userDeslogueado',);
-},
-register: function(req, res, next) {
-  res.render('register',);
-},
-procesarRegister: function (req,res,next){
-  let info = req.body
-  console.log(info.email);
-  let passEncriptada = bcryptjs.hashSync (info.contrasenia,10);
-  let fotoDePerfil = req.file.filename;
-  let usuarioParaGuardar = {
-    nombre : info.nombre,
-    apellido: info.apellido,
-    email : info.email,
-    usuario: info.usuario,
-    contrasenia: passEncriptada,
-    fechadeNacimiento: info.fechaNacimiento,
-    dni: info.dni,
-    fotoDePerfil: info.fotoDePerfil,
-    seguidores:200,
-    comentarios:450,
-    productosSubidos:3,
-    remember_token : "false",
-    created_at: new Date (),
-    updated_at: new Date(),
-    fotoPerfil : fotoDePerfil
+  userDeslogueado: function (req, res, next) {
+
+    res.render('userDeslogueado',);
+  },
+  register: function (req, res, next) {
+    res.render('register',);
+  },
+  procesarRegister: function (req, res, next) {
+    let info = req.body
+    console.log(info.email);
+    let passEncriptada = bcryptjs.hashSync(info.contrasenia, 10);
+    let fotoDePerfil = req.file.filename;
+    let usuarioParaGuardar = {
+      nombre: info.nombre,
+      apellido: info.apellido,
+      email: info.email,
+      usuario: info.usuario,
+      contrasenia: passEncriptada,
+      fechadeNacimiento: info.fechaNacimiento,
+      dni: info.dni,
+      fotoDePerfil: info.fotoDePerfil,
+      seguidores: 200,
+      comentarios: 450,
+      productosSubidos: 3,
+      remember_token: "false",
+      created_at: new Date(),
+      updated_at: new Date(),
+      fotoPerfil: fotoDePerfil
+    }
+
+    user.create(usuarioParaGuardar)
+      .then((result) => {
+        return res.redirect("/users/login")
+      })
+
+  },
+
+  login: function (req, res, next) {
+    res.render('login',);
+  },
+
+  procesarLogin: function (req, res) {
+    let info = req.body
+    user.findOne({
+      where: [email = info.email]
+    })
+    .then((result) => {
+        if (result != null) {
+          let claveCorrecta = bcryptjs.compareSync(info.password, result.password)
+          if (claveCorrecta) {
+            return res.send("hola")
+          } else {
+            return res.send("Existe el mail " + result.email + " pero la clave es incorrecta")
+          }
+
+        } else {
+          return res.send("No existe el mail " + info.email)
+        }
+    });
+  },
+
+  editar: function (req, res) {
+    res.render('profile-edit', {
+      //usuarioLogueado:data.usuario
+    })
   }
-
-  user.create(usuarioParaGuardar)
-  .then((result)=>{
-    return res.redirect("/users/login")
-  })
-  
-},
-
-login: function(req, res, next) {
-  res.render('login',);
-},
-
-procesarLogin: function (req,res){
-let info = req.body
-user.findOne({
-  where : [ email = info.email]
- })
- .then((result)=>{
-   if(result != null){
-     let claveCorrecta = bcryptjs.compareSync(info.password,result.password)
-     if( claveCorrecta ){
-       req.session.user = result.dataValues 
-       console.log(req.session.user)
-     }
-   }
-
- })
-
-},
-
-editar: function(req,res){
-  res.render('profile-edit',{
-    //usuarioLogueado:data.usuario
-  })
-}
 
 
 }
